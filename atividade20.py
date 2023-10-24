6f46c2483741daa506a8f75ebcee6514a057890b
MarianaMTF <142687755+MarianaMTF@users.noreply.github.com>
2023-10-24 11:19:43 -0300

    Create atividade20.py

---
+++ atividade20.py
@@ -0,0 +1,5 @@
+def ordem(animais):
+  animais=sorted(animais)
+  print(f"{animais[0]}, {animais[1]}, {animais[2]}")
+animais=("leão", "tigre", "cachorro", "gato", "elefante", "panda", "girafa", "papagaio", "coelho", "rato")
+ordem(animais)
