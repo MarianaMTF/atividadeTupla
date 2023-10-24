cda235610ed05b1a27f0f81c715a531936a74c66
MarianaMTF <142687755+MarianaMTF@users.noreply.github.com>
2023-10-24 00:41:06 -0300

    Create atividade8.py

---
+++ atividade8.py
@@ -0,0 +1,6 @@
+numero_usu=input("digite um numero:")
+numeros=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22)
+if numero_usu in numeros:
+  print("numero encontrado")
+else:
+  print("numero não encontrado")
